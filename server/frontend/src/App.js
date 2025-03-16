ba5f384d800df8b5faa9da9610892c360891c02c
RenceDev <rence.shane15@gmail.com>
2025-03-16 11:59:23 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,12 +1,15 @@
-import Login from "./components/Login/Login";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login/Login';
+import Register from './components/Register/Register';  // Import Register component
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<Login />} />  {/* Login route for React */}
+      <Route path="/login" element={<Login />} />  {/* Login route */}
+      <Route path="/register" element={<Register />} />  {/* Register route */}
       {/* Other routes */}
     </Routes>
   );
 }
+
 export default App;
